1d711ce05f3d4e63a9f1dffde19a941e93d24bef
Mehul Tikekar <mehul.tikekar@analog.com>
2017-12-08 14:58:05 -0500

    Fix Mac setup

--- functions/mac_setup.fish
+++ functions/mac_setup.fish
@@ -1,7 +1,7 @@
 function mac_setup
-    source ~/.local/opt/modules-tcl/v1.923/init/fish
     module add use.own
     module add gnucoreutils
     module add dotlocal
     module add python
+    module add editerm
 end
--- functions/module.fish
+++ functions/module.fish
@@ -1,4 +1,8 @@
-function module
-    eval (modulecmd tcsh $argv)
+switch (uname)
+    case Linux
+        function module
+            eval (modulecmd tcsh $argv)
+        end
+    case Darwin
+        source ~/.local/opt/modules-tcl/v1.923/init/fish
 end
-
