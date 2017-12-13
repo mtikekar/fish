25f36fa60e465784c75e3fac7ac157edc1e62834
Mehul Tikekar <mehul.tikekar@analog.com>
2017-12-12 19:00:59 -0500

    test valid inputs on conda function

--- functions/conda.fish
+++ functions/conda.fish
@@ -3,22 +3,38 @@ function _conda_env_path
 end
 
 function conda -w conda
-    switch $argv[1]
+    switch "$argv[1]"
     case activate
+        test (count $argv) -eq 2; or begin
+            echo "Expected: conda activate <env>" 1>&2
+            return 1
+        end
+
+        set env_name $argv[2]
+        set env_path (_conda_env_path $env_name)
+        test -n "$env_path"; or begin
+            echo "conda env `$env_name` not found" 1>&2
+            return 1
+        end
+
         # remove old environment
         set -q CONDA_PREFIX; and pop PATH $CONDA_PREFIX/bin
 
         # add new environment
-        set env_name $argv[2]
-        set env_path (_conda_env_path $env_name)
         set -gx CONDA_DEFAULT_ENV $env_name
         set -gx CONDA_PREFIX $env_path
         set PATH $env_path/bin $PATH
     case deactivate
+        test -n "$CONDA_PREFIX"; or begin
+            echo "No conda env currently set"
+            return 1
+        end
+
         pop PATH $CONDA_PREFIX/bin
         set -e CONDA_PREFIX
         set -e CONDA_DEFAULT_ENV
     case '*'
         command conda $argv
     end
+    return 0
 end
