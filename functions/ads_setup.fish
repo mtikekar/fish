56f8c50ecf1fe0d973fae2f62947aaa995abf07c
Mehul Tikekar <mehul.tikekar@analog.com>
2018-06-04 14:40:22 -0400

    fix module function for warnings

--- functions/ads_setup.fish
+++ functions/ads_setup.fish
@@ -1,11 +1,14 @@
 function _module_py
     /usr/bin/python -c "
-import os, subprocess, sys, string
+import os, subprocess, sys
 
 class MyEnv(dict):
     def __setitem__(self, key, val):
         super(MyEnv, self).__setitem__(key, val)
-        print('setenv %s \"%s\"' % (key, val))
+        print 'setenv %s \"%s\"' % (key, val)
+
+    def has_key(self, key):
+        return False if key == 'ADIM_WARNINGS' else super(MyEnv, self).has_key(key)
 
 os.environ['ADIM_PATH_TO_MODULES'] = '/usr/cadtools/bin/modules.dir'
 os.environ = MyEnv(os.environ)
@@ -13,6 +16,8 @@ cmd = ['/usr/cadtools/bin/modules.dir/module', 'python'] + sys.argv[1:]
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 commands, _ = proc.communicate()
 exec commands
+if dict.has_key(os.environ, 'ADIM_WARNINGS'):
+    sys.stderr.write(os.environ['ADIM_WARNINGS'].replace('#', '\n'))
 " $argv
 end
 
