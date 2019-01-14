60ee6c948f8cf6c744528d5a1c1477b11853cad4
Mehul Tikekar <mehul.tikekar@analog.com>
2019-01-14 14:58:33 -0500

    refactor fish_prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -3,13 +3,16 @@ function fish_prompt --description 'Write out the prompt'
     set -q CONDA_DEFAULT_ENV; and printf "(%s) " $CONDA_DEFAULT_ENV
     set -q JULIA_PROJECT; and printf "(%s) " (basename $JULIA_PROJECT)
 
-    if set -q TMUX
-        tmux refresh -S
+    set -l git
+    if set -q fish_no_git
+        set git ""
     else if df . -t osxfuse -t fuse.sshfs > /dev/null 2> /dev/null
-        printf "%s%s:%s%s%s %s" (set_color green) $_prompt_hostname (set_color blue) (prompt_pwd) (set_color yellow) "(fuse)"
+        set git "(fuse)"
     else
-        printf "%s%s:%s%s%s %s" (set_color green) $_prompt_hostname (set_color blue) (prompt_pwd) (set_color yellow) (__fish_git_prompt '(%s)')
+        set git (__fish_git_prompt '(%s)')
     end
+
+    printf "%s%s:%s%s%s %s" (set_color green) $_prompt_hostname (set_color blue) (prompt_pwd) (set_color yellow) $git
 
     if [ $last_status = 0 ]
         printf '%s$ ' (set_color normal)
